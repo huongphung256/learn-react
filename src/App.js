78880575a1efae9c1715eb5cbbc983917e297228
huongphung256 <phunghuuhuong256@gmail.com>
2021-02-01 00:57:35 +0700

    class components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet';
+import React, { Components } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <Greet />
-    </div>
-  );
+import Greet from './components/Greet';
+import Welcome from './components/Welcome'
+
+class App extends Components {
+  render() {
+    return (
+      <div className="App">
+        <Greet />
+        <Welcome />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React, { Components } from 'react'
+
+class Welcome extends Components {
+    render() {
+        return <h1>Class components</h1>
+    }
+}
+
+export default Welcome
